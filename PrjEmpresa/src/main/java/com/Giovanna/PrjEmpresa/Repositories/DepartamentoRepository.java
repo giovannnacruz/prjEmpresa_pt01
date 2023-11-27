781176e11cc572d085a3941a2c90747088844806
giovanna cristina <133894598+giovannnacruz@users.noreply.github.com>
2023-11-27 13:48:49 -0300

    Delete PrjEmpresa directory

--- PrjEmpresa/src/main/java/com/Giovanna/PrjEmpresa/Repositories/DepartamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Giovanna.PrjEmpresa.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Giovanna.PrjEmpresa.entities.Departamento;
-
-public interface DepartamentoRepository extends JpaRepository<Departamento, Long>{
-
-}
--- PrjEmpresa/src/main/java/com/Giovanna/PrjEmpresa/Repositories/FuncionarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Giovanna.PrjEmpresa.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Giovanna.PrjEmpresa.entities.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-	
-}
